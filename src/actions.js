0dad1ca733ec7bd1cc0fa2e4a1fd36915ad00db0
Josh Loya <vandor@users.noreply.github.com>
2018-09-27 11:33:11 -0400

    Remove weird invisible characters

--- src/actions.js
+++ src/actions.js
@@ -11,11 +11,11 @@ export const visibilityFilters = {
 export function addTodo(text) {
   return { type: ADD_TODO, text }
 }
-​
+
 export function toggleTodo(index) {
   return { type: TOGGLE_TODO, index }
 }
-​
+
 export function setVisibilityFilter(filter) {
   return { type: SET_VISIBILITY_FILTER, filter }
 }
