644c92806166142486080e3db256705607226bbe
Josh Loya <vandor@users.noreply.github.com>
2018-09-27 11:53:23 -0400

    Create store and log some changes to observe.
    Include extension for redux devtools

--- src/index.js
+++ src/index.js
@@ -5,10 +5,29 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import { createStore } from 'redux';
-// import todoApp from './reducers'
-// const store = createStore(todoApp)
+import todoApp from './reducers'
+import {
+  addTodo,
+  toggleTodo,
+  setVisibilityFilter,
+  VisibilityFilters,
+} from './actions'
 
-// console.log(store.getState())
+const store = createStore(
+  todoApp,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+console.log(store.getState())
+const unsubscribe = store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(addTodo('Learn about actions'))
+store.dispatch(addTodo('Learn about reducers'))
+store.dispatch(addTodo('Learn about store'))
+store.dispatch(toggleTodo(0))
+store.dispatch(toggleTodo(1))
+store.dispatch(setVisibilityFilter(VisibilityFilters.SHOW_COMPLETED))
+unsubscribe()
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
