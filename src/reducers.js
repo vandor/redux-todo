ecf5c86b47c43e2bec7e963cffeb3184879e2b37
Josh Loya <vandor@users.noreply.github.com>
2018-09-27 10:03:41 -0400

    Add todo actions and reducers.
    We have also begun to extract a reducer for the todos list.

---
+++ src/reducers.js
@@ -0,0 +1,58 @@
+import {
+  ADD_TODO,
+  TOGGLE_TODO,
+  SET_VISIBILITY_FILTER,
+  visibilityFilters
+} from './actions'
+
+const initialState = {
+  visibilityFilter: visibilityFilters.SHOW_ALL,
+  todos: [],
+}
+
+function todos(state, action) {
+  switch(action.type) {
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+        ...state.todos,
+        {
+          text: action.text,
+          completed: false,
+        }
+        ]
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: state.todos.map((todo, index) => {
+          if (index === action.index) {
+            return Object.assign({}, todo, {
+              completed: !todo.completed,
+            });
+          }
+          return todo;
+        })
+      })
+    default:
+      return state;
+  }
+}
+
+function todoApp(state = initialState, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter,
+      })
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      })
+    case TOGGLE_TODO:
+      return Object.assign({}, state, {
+        todos: todos(state.todos, action)
+      })
+    default:
+      return state
+  }
+}
