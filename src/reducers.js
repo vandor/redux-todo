2493cdf5c4630e2b24e503c4a01027f84c102e7d
Josh Loya <vandor@users.noreply.github.com>
2018-09-27 10:14:48 -0400

    Extract reducer for visibilityFilter as well

--- src/reducers.js
+++ src/reducers.js
@@ -5,54 +5,44 @@ import {
   visibilityFilters
 } from './actions'
 
-const initialState = {
-  visibilityFilter: visibilityFilters.SHOW_ALL,
-  todos: [],
+const { SHOW_ALL } = VisibilityFilters
+
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter;
+    default:
+      return state;
+  }
 }
 
-function todos(state, action) {
-  switch(action.type) {
+function todos(state = [], action) {
+  switch (action.type) {
     case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: [
-        ...state.todos,
+      return [
+        ...state,
         {
           text: action.text,
           completed: false,
         }
-        ]
-      })
+      ]
     case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: state.todos.map((todo, index) => {
-          if (index === action.index) {
-            return Object.assign({}, todo, {
-              completed: !todo.completed,
-            });
-          }
-          return todo;
-        })
+      return state.map((todo, index) => {
+        if (index === action.index) {
+          return Object.assign({}, todo, {
+            completed: !todo.completed,
+          });
+        }
+        return todo;
       })
     default:
       return state;
   }
 }
 
-function todoApp(state = initialState, action) {
-  switch (action.type) {
-    case SET_VISIBILITY_FILTER:
-      return Object.assign({}, state, {
-        visibilityFilter: action.filter,
-      })
-    case ADD_TODO:
-      return Object.assign({}, state, {
-        todos: todos(state.todos, action)
-      })
-    case TOGGLE_TODO:
-      return Object.assign({}, state, {
-        todos: todos(state.todos, action)
-      })
-    default:
-      return state
+function todoApp(state = {}, action) {
+  return {
+    visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+    todos: todos(state.todos, action),
   }
 }
