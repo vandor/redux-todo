33e8e62a17ffd68369a760061168293239572223
Josh Loya <vandor@users.noreply.github.com>
2018-09-27 11:03:53 -0400

    Create store

--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import { createStore } from 'redux';
+import todoApp from './reducers'
+const store = createStore(todoApp)
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
