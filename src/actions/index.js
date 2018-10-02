07a16a1e64664748249ddd891c9de6105eabb8da
Josh Loya <vandor@users.noreply.github.com>
2018-10-02 10:35:57 -0400

    Add id key to todo state.
    Also move actions to directory

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,14 +8,19 @@ export const VisibilityFilters = {
   SHOW_ACTIVE: 'SHOW_ACTIVE',
 }
 
-export function addTodo(text) {
-  return { type: ADD_TODO, text }
-}
+let nextTodoId = 0
+export const addTodo = text => ({
+  type: ADD_TODO,
+  id: nextTodoId++,
+  text,
+})
 
-export function toggleTodo(index) {
-  return { type: TOGGLE_TODO, index }
-}
+export const toggleTodo = id => ({
+  type: TOGGLE_TODO,
+  id,
+})
 
-export function setVisibilityFilter(filter) {
-  return { type: SET_VISIBILITY_FILTER, filter }
-}
+export const setVisibilityFilter = filter => ({
+  type: SET_VISIBILITY_FILTER,
+  filter,
+})
